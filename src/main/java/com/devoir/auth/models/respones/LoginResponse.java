5bcd38fa2b3b3d074091c2bec1b44ecb79990db4
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-12-12 00:19:40 +0100

    Update LoginResponse.java

--- src/main/java/com/devoir/auth/models/respones/LoginResponse.java
+++ src/main/java/com/devoir/auth/models/respones/LoginResponse.java
@@ -1,18 +1,18 @@
 package com.devoir.auth.models.respones;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @RequiredArgsConstructor
+@Setter
 public class LoginResponse {
-    @Setter private String jwt;
+    private String jwt;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
-    @Setter private String name;
-    @Setter private String email;
+    private String name;
+    private String email;
 }
