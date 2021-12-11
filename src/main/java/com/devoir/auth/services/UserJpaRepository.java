1afee1ef088c2023898c24701dc1006203278250
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-12-12 00:12:47 +0100

    Update UserJpaRepository.java

--- src/main/java/com/devoir/auth/services/UserJpaRepository.java
+++ src/main/java/com/devoir/auth/services/UserJpaRepository.java
@@ -3,5 +3,8 @@ package com.devoir.auth.services;
 import com.devoir.auth.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserJpaRepository extends JpaRepository<User,Long> {
+    Optional<User> findFirstByEmail(String email);
 }
