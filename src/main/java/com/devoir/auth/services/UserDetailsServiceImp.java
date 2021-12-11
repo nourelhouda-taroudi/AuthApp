5f060cdb520ba39a5eb5505c1293ca2aefab688d
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-12-12 00:23:17 +0100

    updated loaduser method

--- src/main/java/com/devoir/auth/services/UserDetailsServiceImp.java
+++ src/main/java/com/devoir/auth/services/UserDetailsServiceImp.java
@@ -1,6 +1,7 @@
 package com.devoir.auth.services;
 
 
+import com.devoir.auth.models.MyUserDetails;
 import com.devoir.auth.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,20 +10,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class UserDetailsServiceImp implements UserDetailsService {
     @Autowired
-    private UserRepository userRepository;
+    private UserJpaRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository
-                .findFirstByEmail(email)
-                .orElseThrow(
-                        () -> new UsernameNotFoundException(String.format("Username %s not found.", email)));
+        Optional<User> user= userRepository.findFirstByEmail(email);
 
+        user.orElseThrow(()->new UsernameNotFoundException("Not found:" + email));
+        final MyUserDetails myUserDetails = user.map(MyUserDetails::new).get();
+
+        return myUserDetails;
     }
-
-
 }
